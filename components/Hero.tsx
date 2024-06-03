92aad56232c2a1157ad9d57b3069a08a842346f6
kylectrent <kylectrent@gmail.com>
2024-06-03 15:23:56 -0700

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 export const Hero = () => {
   return (
     <div>
-        <Typography variant='h1'>Functional Mushroom Beverages</Typography>
-        <Typography variant='h2'>Functional Mushroom Beverages</Typography>
+        <Typography variant='h1' textTransform={'uppercase'} fontSize={'1.5rem'} letterSpacing={'0.5rem'} color='#a1ff00'>Functional Mushroom Beverages</Typography>
+        <Typography variant='h2' fontWeight={'800'}>For your mouth to drink, so drink them</Typography>
     </div>
   )
 }
