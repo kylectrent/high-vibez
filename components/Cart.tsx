17a0a9746fc07edb97dfef3b44f12a7152db210d
kylectrent <kylectrent@gmail.com>
2024-11-16 17:07:27 -0800

    cart to nav

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,7 +1,25 @@
+import { Box } from '@mui/material';
 import React from 'react';
 
-const Cart = () => {
-  return <div>Cart</div>;
+export const Cart = () => {
+  return (
+    <Box
+      sx={{
+        position: 'absolute',
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: 'rgba(255, 0, 0, 0.5)',
+      }}
+    >
+      <Box
+        sx={{
+          position: 'absolute',
+          right: 300,
+          minHeight: '20vh',
+          minWidth: '50vw',
+          backgroundColor: 'white',
+        }}
+      ></Box>
+    </Box>
+  );
 };
-
-export default Cart;
