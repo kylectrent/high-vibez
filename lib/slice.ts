3b9592d8c9bd3fd8505e25590b10748250117aaf
kylectrent <kylectrent@gmail.com>
2025-01-03 09:33:04 -0800

    set up boilerplate redux

---
+++ lib/slice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+import counterReducer from './slice'
+
+interface CounterState {
+  value: number
+}
+
+const initialState = { value: 0 } satisfies CounterState as CounterState
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.value++
+    },
+    decrement(state) {
+      state.value--
+    },
+    incrementByAmount(state, action: PayloadAction<number>) {
+      state.value += action.payload
+    },
+  },
+})
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export default counterSlice.reducer
